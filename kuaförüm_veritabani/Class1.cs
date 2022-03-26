ae841a2fdc95ce25deb09249b1f2c2dda2690f9b
lenova <lenova@LAPTOP-NSGP96LJ>
2022-03-26 19:55:45 +0300

    Proje dosyası ekle.

---
+++ kuaförüm_veritabani/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kuaförüm_veritabani
+{
+    public class Class1
+    {
+    }
+}
